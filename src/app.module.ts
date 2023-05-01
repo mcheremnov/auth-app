371f6a56d86d3dc973eb313dd335feef60ad902d
mcheremnov <cheremnovmaksym@gmail.com>
2023-05-01 20:27:40 +0300

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -21,6 +22,10 @@ import { User } from './users/users.entities';
       entities: [User],
       synchronize: true,
     }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    })
   ],
   controllers: [AppController],
   providers: [AppService, UsersService],
