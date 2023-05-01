5b2a34b84503a28e75a3f20b1f21c4c6f870ee77
mcheremnov <cheremnovmaksym@gmail.com>
2023-05-01 20:40:33 +0300

    Add guards services

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.getToken(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET
+      });
+      request['user'] = payload;
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
+    return
+  }
+
+  private getToken(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
