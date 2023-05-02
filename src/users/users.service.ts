7eb6cbc0ecb5a647a02558160ef815d0ae153530
mcheremnov <cheremnovmaksym@gmail.com>
2023-05-02 15:55:22 +0300

    Fix errors

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,6 +7,10 @@ import { User } from './users.entities';
 export class UsersService {
   constructor(@InjectRepository(User) private userRepo: Repository<User>,) { }
 
+  async findAll(): Promise<User[]> {
+    return this.userRepo.find();
+  }
+
   async findOne(email: string): Promise<User | void> {
     return await this.userRepo.findOne({ where: [{ email: email }] })
       .catch(err => console.error('Error finding user with email %s /n Error msg: %s', email, err));
@@ -16,15 +20,11 @@ export class UsersService {
     return this.userRepo.create(user);
   }
 
-  async updateUser(id: number, user: Partial<User>): Promise<User> {
-    const dbUser = await this.userRepo.findOneById(id);
-    if (dbUser) {
-      return await this.userRepo.update(id, user);
-    }
-    return new Error("Failed to find user with id %s", id);
+  async updateUser(id: number, user: Partial<User>) {
+    return await this.userRepo.update(id, user);
   }
 
-  async deleteUser(id: number): bool {
+  async deleteUser(id: number): Promise<boolean> {
     const result = await this.userRepo.delete(id);
     if (result) {
       return true;
